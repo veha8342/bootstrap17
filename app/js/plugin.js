cfd7701ce7147a7d27ad2a140e184fe75a810575
veha8342 <veha1986@gmail.com>
2017-09-04 23:27:31 +0300

    adaptive

--- app/js/plugin.js
+++ app/js/plugin.js
@@ -1,6 +1,10 @@
-;function ($) {
-     var header = $("header");
+;(function ($) {
+     var header = $('header');
      $(window).on('scroll', function () {
-         
+         if( $(window).scrollTop() > 20){
+              header.addClass('on-scroll');
+         } else {
+                  header.removeClass('on-scroll');
+         }
      })
 })(jQuery);
